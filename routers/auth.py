e684af4f10d8a11b9eafaee3454749d855bffd32
Patryk Dudka <dudekzwp@gmail.com>
2024-03-11 22:26:55 +0100

    added register feature

--- routers/auth.py
+++ routers/auth.py
@@ -33,3 +33,39 @@ async def login_page(request: Request):
 @router.get("/register", response_class=HTMLResponse)
 async def login_page(request: Request):
     return templates.TemplateResponse("register.html", {"request": request})
+
+
+@router.post("/register", response_class=HTMLResponse)
+async def register_user(request: Request, email: str = Form(...), username: str = Form(...),
+                        firstname: str = Form(...), lastname: str = Form(...),
+                        password: str = Form(...), password2: str = Form(...),
+                        db: Session = Depends(get_db)):
+
+    # checks if username in DB
+    validation1 = db.query(models.User).filter(
+        models.User.username == username).first()
+
+    # checks if email in DB
+    validation2 = db.query(models.User).filter(
+        models.User.email == email).first()
+
+    if password != password2:
+        msg = "Verify password must be the same"
+        return templates.TemplateResponse("register.html", {"request": request, "msg": msg})
+    if validation1 is not None or validation2 is not None:
+        msg = "Username or Email is already in use"
+        return templates.TemplateResponse("register.html", {"request": request, "msg": msg})
+
+    user_model = models.User()
+    user_model.username = username
+    user_model.email = email
+    user_model.first_name = firstname
+    user_model.last_name = lastname
+
+    user_model.hashed_password = password
+
+    db.add(user_model)
+    db.commit()
+
+    msg = "User successfully created"
+    return templates.TemplateResponse("login.html", {"request": request, "msg": msg})
