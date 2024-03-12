4beeed4d40f7b33e8363a07ac722b45d16815d36
Patryk Dudka <dudekzwp@gmail.com>
2024-03-12 13:27:03 +0100

    Added login redirection depending on user role

--- routers/admin.py
+++ routers/admin.py
@@ -5,7 +5,7 @@ from starlette.staticfiles import StaticFiles
 from fastapi import Depends, HTTPException, status, APIRouter, Request, Response, Form
 import models
 from database import SessionLocal, engine
-from utils import get_db
+from utils import get_db, check_user_role_and_redirect
 
 templates = Jinja2Templates(directory="templates")
 
@@ -19,6 +19,11 @@ router = APIRouter(
 
 
 @router.get("/", response_class=HTMLResponse)
-async def home_page(request: Request):
+async def home_page(request: Request, db: Session = Depends(get_db)):
+
+    # checks if customer is logged in (if different role then redirection)
+    redirection = check_user_role_and_redirect(request, db, 'admin')
+    if redirection["is_needed"]:
+        return redirection['redirection']
 
     return templates.TemplateResponse("admin.html", {"request": request})
