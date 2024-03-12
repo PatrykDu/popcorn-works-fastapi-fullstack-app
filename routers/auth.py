6a04f21d6d23d88fbdb3ea3d871aa5db29ab1669
Patryk Dudka <dudekzwp@gmail.com>
2024-03-12 11:45:55 +0100

    Added customer login

--- routers/auth.py
+++ routers/auth.py
@@ -1,13 +1,25 @@
+import os
+from typing import Optional
+from starlette.responses import RedirectResponse
 from fastapi.responses import HTMLResponse
+from passlib.context import CryptContext
 from sqlalchemy.orm import Session
+from datetime import datetime, timedelta
 from fastapi.templating import Jinja2Templates
 from starlette.staticfiles import StaticFiles
 from fastapi import Depends, HTTPException, status, APIRouter, Request, Response, Form
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 import models
 from database import SessionLocal, engine
 from utils import get_db
+from jose import jwt, JWTError
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
+ALGORITHM = "HS256"
 
 templates = Jinja2Templates(directory="templates")
+
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -17,27 +29,77 @@ router = APIRouter(
 )
 
 
+# def get_password_hash(password):
+#     return bcrypt_context.hash(password)
+
+
+# def verify_password(plain_password, hashed_password):
+#     return bcrypt_context.verify(plain_password, hashed_password)
+
+
+def authenticate_user(username: str, password: str, db):
+    user = db.query(models.User).filter(
+        models.User.username == username).first()
+
+    if not user:
+        return False
+    if password != user.hashed_password:
+        return False
+    return user
+
+
+def create_access_token(username: str, user_id: int,
+                        expires_delta: Optional[timedelta] = None):
+
+    encode = {"sub": username, "id": user_id}
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    encode.update({"exp": expire})
+    return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+@router.post("/token")
+async def login_for_access_token(response: Response, form_data: OAuth2PasswordRequestForm = Depends(),
+                                 db: Session = Depends(get_db)):
+    user = authenticate_user(form_data['username'], form_data['password'], db)
+    if not user:
+        return False
+    token_expires = timedelta(minutes=60)
+    token = create_access_token(user.username,
+                                user.id,
+                                expires_delta=token_expires)
+
+    response.set_cookie(key="access_token", value=token, httponly=True)
+
+    return True
+
+
 @router.get("/login", response_class=HTMLResponse)
 async def login_page(request: Request):
     return templates.TemplateResponse("login.html", {"request": request})
 
 
 @router.post("/login", response_class=HTMLResponse)
-async def register_user(request: Request, username: str = Form(...),
-                        password: str = Form(...), db: Session = Depends(get_db)):
+async def login_user(request: Request, username: str = Form(...), password: str = Form(...), db: Session = Depends(get_db)):
+    try:
 
-    # checks if username exist
-    found_user_model = db.query(models.User).filter(
-        models.User.username == username).first()
-    if found_user_model is None:
-        msg = "Username is not registered"
+        form_data = {"request": request,
+                     "username": username, "password": password}
+
+        response = RedirectResponse(
+            url="/customer", status_code=status.HTTP_302_FOUND)
+
+        validate_user_cookie = await login_for_access_token(response=response, form_data=form_data, db=db)
+
+        if not validate_user_cookie:
+            msg = "Incorrect Username or Password"
+            return templates.TemplateResponse("login.html", {"request": request, "msg": msg})
+        return response
+    except HTTPException:
+        msg = "Unknown Error"
         return templates.TemplateResponse("login.html", {"request": request, "msg": msg})
-
-    if found_user_model.hashed_password != password:
-        msg = "Wrong Password!"
-        return templates.TemplateResponse("login.html", {"request": request, "msg": msg})
-
-    return templates.TemplateResponse("customer.html", {"request": request})
 
 
 @router.get("/register", response_class=HTMLResponse)
