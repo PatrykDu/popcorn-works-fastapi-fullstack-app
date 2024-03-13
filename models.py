24f4f78166439422c1bf9f8e47368e489994afdd
Patryk Dudka <patrykdudka9@gmail.com>
2024-03-13 09:32:37 +0100

    Login register feature and full redirection functionality (#6)
    * added register feature
    
    * Added post login
    
    * Added utils.py
    
    * Added mechanic endpoint
    
    * Added customer login
    
    * Added admin endpoint
    
    * Added redirection
    
    * Added login redirection depending on user role
    
    * Added full redirection feature
    
    * Added wrong password handling
    
    * Linting
    
    * Added wrong endpoint handling
    
    * Added docstrings

--- models.py
+++ models.py
@@ -4,6 +4,10 @@ from database import Base
 
 
 class User(Base):
+    """User table in database. We can store 3 types of user.
+    customer - default user created by register endpoint
+    mechanic - superuser providing services for the customers
+    admin - admin account using admin endpoint as his default"""
     __tablename__ = "user"
 
     id = Column(Integer, primary_key=True, index=True)
@@ -12,3 +16,4 @@ class User(Base):
     first_name = Column(String)
     last_name = Column(String)
     hashed_password = Column(String)
+    role = Column(String, default='customer')
---
+++ routers/admin.py
@@ -0,0 +1,32 @@
+from fastapi.responses import HTMLResponse
+from sqlalchemy.orm import Session
+from fastapi.templating import Jinja2Templates
+from starlette.staticfiles import StaticFiles
+from fastapi import Depends, HTTPException, status, APIRouter, Request, Response, Form
+import models
+from database import SessionLocal, engine
+from utils import get_db, check_user_role_and_redirect, get_current_user
+
+templates = Jinja2Templates(directory="templates")
+
+models.Base.metadata.create_all(bind=engine)
+
+router = APIRouter(
+    prefix="/admin",
+    tags=["admin"],
+    responses={401: {"user": "Not authorized"}}
+)
+
+
+@router.get("/", response_class=HTMLResponse)
+async def home_page(request: Request, db: Session = Depends(get_db)):
+    """Get request for starting admin page after beeing logged in"""
+
+    # checks if customer is logged in (if different role then redirection)
+    redirection = check_user_role_and_redirect(request, db, 'admin')
+    if redirection["is_needed"]:
+        return redirection['redirection']
+
+    user = get_current_user(request)
+
+    return templates.TemplateResponse("admin.html", {"request": request, "user": user})
