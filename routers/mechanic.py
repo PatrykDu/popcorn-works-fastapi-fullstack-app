bbb239cfad43d8fef871a0307034ebbb5bb29ba3
Patryk Dudka <patrykdudka9@gmail.com>
2024-03-13 20:02:18 +0100

    Added blank pages depended on mechanic page

--- routers/mechanic.py
+++ routers/mechanic.py
@@ -35,3 +35,51 @@ async def home_page(request: Request, db: Session = Depends(get_db)):
     messages = db.query(models.Message).all()
 
     return templates.TemplateResponse("mechanic.html", {"request": request, "user": user, "messages": messages})
+
+
+@router.get("/repairs", response_class=HTMLResponse)
+async def home_page(request: Request, db: Session = Depends(get_db)):
+    """Get request for starting mechanic page after beeing logged in"""
+
+    redirection = check_user_role_and_redirect(request, db, 'mechanic')
+    if redirection["is_needed"]:
+        return redirection['redirection']
+
+    user_decoded = get_current_user(request)
+
+    user = db.query(models.User).filter(
+        models.User.username == user_decoded['username']).first()
+
+    return templates.TemplateResponse("repairs_mechanic.html", {"request": request, "user": user})
+
+
+@router.get("/storage", response_class=HTMLResponse)
+async def home_page(request: Request, db: Session = Depends(get_db)):
+    """Get request for starting mechanic page after beeing logged in"""
+
+    redirection = check_user_role_and_redirect(request, db, 'mechanic')
+    if redirection["is_needed"]:
+        return redirection['redirection']
+
+    user_decoded = get_current_user(request)
+
+    user = db.query(models.User).filter(
+        models.User.username == user_decoded['username']).first()
+
+    return templates.TemplateResponse("storage.html", {"request": request, "user": user})
+
+
+@router.get("/calendar", response_class=HTMLResponse)
+async def home_page(request: Request, db: Session = Depends(get_db)):
+    """Get request for starting mechanic page after beeing logged in"""
+
+    redirection = check_user_role_and_redirect(request, db, 'mechanic')
+    if redirection["is_needed"]:
+        return redirection['redirection']
+
+    user_decoded = get_current_user(request)
+
+    user = db.query(models.User).filter(
+        models.User.username == user_decoded['username']).first()
+
+    return templates.TemplateResponse("mechanic_calendar.html", {"request": request, "user": user})
