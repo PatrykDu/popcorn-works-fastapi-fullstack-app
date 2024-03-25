b8f77b485ff3ab453c69befb85a583cea9c6de3c
Patryk Dudka <patrykdudka9@gmail.com>
2024-03-25 13:35:14 +0100

    Customer endpoint (#24)
    * Added customer endpoint
    
    * Changed language to english

--- routers/customer.py
+++ routers/customer.py
@@ -30,4 +30,50 @@ async def customer_home_page(request: Request, db: Session = Depends(get_db)):
 
     user = get_current_user(request)
 
-    return templates.TemplateResponse("customer.html", {"request": request, "user": user})
+    customer_repairs = db.query(models.Repair).filter(
+        models.Repair.customer_id == user['id']).all()
+
+    return templates.TemplateResponse("customer.html", {"request": request,
+                                                        "user": user,
+                                                        "customer_repairs": customer_repairs})
+
+
+@router.get("/repairs", response_class=HTMLResponse)
+async def customer_home_page(request: Request, db: Session = Depends(get_db)):
+    """Get request for customer/repairs page after beeing logged in"""
+
+    # checks if customer is logged in (if different role then redirection)
+    redirection = check_user_role_and_redirect(request, db, 'customer')
+    if redirection["is_needed"]:
+        return redirection['redirection']
+
+    user = get_current_user(request)
+
+    customer_repairs = db.query(models.Repair).filter(
+        models.Repair.customer_id == user['id']).all()
+
+    return templates.TemplateResponse("repairs_customer.html", {"request": request,
+                                                                "user": user,
+                                                                "customer_repairs": customer_repairs})
+
+
+@router.get("/repairs/{repair_id}", response_class=HTMLResponse)
+async def repairs_id_page_for_mechanic(request: Request, repair_id: int, db: Session = Depends(get_db)):
+    """Get request for repair_id page"""
+
+    redirection = check_user_role_and_redirect(request, db, 'customer')
+    if redirection["is_needed"]:
+        return redirection['redirection']
+    user_decoded = get_current_user(request)
+    user = db.query(models.User).filter(
+        models.User.username == user_decoded['username']).first()
+    used_parts = db.query(models.Part).join(models.PartsInRepair, models.Part.id == models.PartsInRepair.part_id).filter(
+        models.PartsInRepair.repair_id == repair_id).all()
+
+    repair = db.query(models.Repair).filter(
+        models.Repair.id == repair_id).first()
+
+    return templates.TemplateResponse("repairs_customer_id.html", {"request": request,
+                                                                   "user": user,
+                                                                   "used_parts": used_parts,
+                                                                   "repair": repair})
